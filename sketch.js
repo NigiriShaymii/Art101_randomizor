8fa9a60463f9f1562acb7d750deccf5aa70436f3
Johnny Lee <johnny.lee0806@gmail.com>
2020-03-02 22:40:32 -0800

    Finish Randomizer

--- sketch.js
+++ sketch.js
@@ -33,27 +33,56 @@ let superpower = [{
 }];
 
 let randomIndex;
+let animating = false;
+
 
 function setup() {
   createCanvas(500, 500);
   background(200);
+  textSize(40);
+  textAlign(CENTER);
+  text("Click to Randomize", width/2, height/2);
+
 }
 
 function draw() {
+  if (animating == true) {
+    ellipse(random(width), random(height), random(50, 200));
+  }
 }
 
-function mousePressed(){
-  
-  background(random(200, 255));
-  
-  if(superpower[0])
-  {
-  randomIndex = int(random(superpower.length));
-  text(superpower[randomIndex].ability, 50,50 );
-  superpower.splice(randomIndex, 1);
+function randomizer() {
+
+  animating = false;
+  if (superpower[0]) {
+
+    background(random(200, 255));
+    randomIndex = int(random(superpower.length));
+    text(`${superpower[randomIndex].ability}: \nPower Level: ${superpower[randomIndex].power}`, width/2, height/2 );
+    superpower.splice(randomIndex, 1);
+  } else {
+    background(random(200, 255));
+    text("That's all!", width/2, height/2 );
   }
-  else
-  {
-    text("That's all!", 50, 50);
-  }
+}
+
+// function changeBackground{
+//   if(counter <= 5)
+//   {
+//     counter++;
+//     console.log(counter);
+//     background(random(255), random(255),random(255));
+//     setTimeout(changeBackground, 1000);
+//   }
+//   else {
+//
+//   }
+//
+// }
+
+function mousePressed() {
+
+  animating = true;
+  setTimeout(randomizer, 2000);
+
 }
